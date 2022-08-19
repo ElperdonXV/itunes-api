01b55ee6e606a8734c96c54a1d40e68f1cf51cd0
ElperdonXV <davidemartinografico@gmail.com>
2022-08-19 15:36:49 +0200

    progress

--- js/script.js
+++ js/script.js
@@ -2,8 +2,24 @@ const app = new Vue(
     {
         el: '#app',
         data: {
+            queryPath: 'https://itunes.apple.com/search',
+            text: '',
+            reserch: [],
         },
         methods: {
+            callItunes: function(){
+                const parametri= {
+                    term: this.text,
+                    media: this.value,
+                }
+                axios.get(`${this.queryPath}`, { params: parametri }
+                ).then((result)=>{
+                    //this.reserch= result.data.results;
+                    console.log(result.data.results);
+                }).catch((error)=>{
+                    console.log(error);
+                });
+            },
         }
     }
 );
