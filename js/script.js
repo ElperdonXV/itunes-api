9395ea95d4bf80cacf7c435e6a785c28a3cbd634
ElperdonXV <davidemartinografico@gmail.com>
2022-08-24 13:22:29 +0200

    work on scroll, design and loading

--- js/script.js
+++ js/script.js
@@ -11,13 +11,17 @@ const app = new Vue(
             offset: 0,
             firstSearch: 0,
             resultsLength: null,
+            loading: false,
             color: 0, // 0 for light mode, 1 for dark mode
+            textSearch: null,
         },
         methods: {
             callItunes: function(){
+                this.results = null;
                 if(this.counter != null){
                     this.counter = null;
                 }
+                this.textSearch = this.text;
                 const parametri= {
                     term: this.text,
                     media: this.value,
@@ -26,20 +30,24 @@ const app = new Vue(
                     limit: 28,
 
                 }
-                axios.get(`${this.queryPath}`, { params: parametri }
-                ).then((result)=>{
-                    this.results= result.data.results;
-                    this.results.forEach(element => {
-                        element.artworkUrl30 = element.artworkUrl30.replace('30x30', '300x300');
-                        element.artworkUrl60 = element.artworkUrl60.replace('60x60', '600x600');
-                        element.artworkUrl100 = element.artworkUrl100.replace('100x100', '1000x1000');
-                        let array = element.releaseDate.split("T");
-                        element.releaseDate = array[0];
-                    });
-                    this.resultsLength = this.results.length;
-                }).catch((error)=>{
-                    console.log(error);
-                });
+                this.loading = true;
+                
+                    axios.get(`${this.queryPath}`, { params: parametri }
+                        ).then((result)=>{
+                            this.results= result.data.results;
+                            this.results.forEach(element => {
+                                element.artworkUrl30 = element.artworkUrl30.replace('30x30', '300x300');
+                                element.artworkUrl60 = element.artworkUrl60.replace('60x60', '600x600');
+                                element.artworkUrl100 = element.artworkUrl100.replace('100x100', '1000x1000');
+                                let array = element.releaseDate.split("T");
+                                element.releaseDate = array[0];
+                            });
+                            this.loading = false;
+                            this.resultsLength = this.results.length;
+                        }).catch((error)=>{
+                            this.loading = false;
+                            console.log(error);
+                        });
             },
 
             show: function(index){
@@ -55,17 +63,36 @@ const app = new Vue(
             nxtPage: function(){
                 this.offset = this.offset + 28;
                 this.callItunes();
+                setTimeout(()=>{
+                    this.scroll();
+                }, 500);
             },
 
             prvPage: function(){
                 this.offset = this.offset - 28;
                 this.callItunes();
+                setTimeout(()=>{
+                    this.scroll();
+                }, 500);
+            },
+
+            scroll: function(){
+                var element = this.$refs['main'];
+                var top = element.offsetTop;
+                window.scrollTo(0, 1000);
             },
 
             firstCall: function(){
                 this.offset = 0;
                 this.firstSearch = 1;
-                this.callItunes();
+                this.results = null;
+                this.loading = true;
+                setTimeout(() =>{
+                    this.callItunes();
+                }, 1000);
+                setTimeout(()=>{
+                    this.scroll();
+                }, 1500);
             },
 
             theme: function(){
@@ -75,7 +102,8 @@ const app = new Vue(
                 else{
                     this.color = 0;
                 }
-            }
+            }, 
+            
         }
     }
 );
